c2e34faf6995a3d8ed51e5beeca0f533b6666bdd
Vinícius Oliveira de Aguiar <vinicius.aguiar@yandeh.com.br>
2022-06-27 16:12:27 -0300

    TDD.

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,3 +1,4 @@
+from typing import List, Union
 
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
@@ -10,3 +11,15 @@ async def post(payload: SummaryPayloadSchema) -> int:
     )
     await summary.save()
     return summary.id
+
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary
+    return None
+
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries

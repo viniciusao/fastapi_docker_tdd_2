bb16e779997e205e307ea6aff9108a3dcd7d1497
Vinícius Oliveira de Aguiar <vinicius.aguiar@yandeh.com.br>
2022-06-28 13:14:02 -0300

    fix: blank lines.

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -2,4 +2,3 @@ def test_ping(test_app):
     response = test_app.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
-
